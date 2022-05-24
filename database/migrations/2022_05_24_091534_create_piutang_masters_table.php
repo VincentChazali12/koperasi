afc5a95fd2243dc3323335203af1ddafc01c1ca3
VincentChazali <vincent19ti@mahasiswa.pcr.ac.id>
2022-05-24 16:27:34 +0700

    latest update

---
+++ database/migrations/2022_05_24_091534_create_piutang_masters_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePiutangMastersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('piutang_masters', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('usulan');
+            $table->integer('sisa');
+            $table->integer('waktu');
+            $table->string('status',100);
+            $table->string('id_anggota');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('piutang_masters');
+    }
+}
